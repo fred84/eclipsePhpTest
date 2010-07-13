5d5497656ee13279073132b51c121750d63189c2
dev03 <dev03@dev03.moscow.chronopay.com>
2010-07-13 17:18:52 +0400

    command binding updated

--- plugin/src/me/galkin/eclipse/php/handler/Handler.java
+++ plugin/src/me/galkin/eclipse/php/handler/Handler.java
@@ -1,134 +1,115 @@
 package me.galkin.eclipse.php.handler;
 
-import java.util.List;
-
 import me.galkin.eclipse.php.PHPUnitPlugin;
 import me.galkin.eclipse.php.domain.IExecAnalyzer;
 import me.galkin.eclipse.php.domain.PHPUnitCommand;
 import me.galkin.eclipse.php.domain.PHPUnitExecAnalyzer;
 import me.galkin.eclipse.php.domain.ProjectFinder;
-import me.galkin.eclipse.php.domain.ProjectNotFoundException;
 import me.galkin.eclipse.php.ui.ResultView;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.expressions.IEvaluationContext;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.dltk.core.IModelElement;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.php.internal.core.documentModel.dom.IImplForPhp;
+import org.eclipse.php.internal.ui.editor.PHPStructuredEditor;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.handlers.HandlerUtil;
 
+@SuppressWarnings("restriction")
 public class Handler extends AbstractHandler {
 
-	private IExecAnalyzer analyzer = new PHPUnitExecAnalyzer();
+	private final IExecAnalyzer analyzer = new PHPUnitExecAnalyzer();
 	
-	
-	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		if (isAlreadyRunning()) {
-			MessageDialog.openInformation(HandlerUtil.getActiveShell(event),
-					"Information", "Another test already running");
-			return null;
-		}
-		
-		ResultView view = getResultView(event);
-
-		if (null == view) {
-			return null;
-		}
-
-		IModelElement unit = getSelection(event);
-
-		if (null == unit) {
-			MessageDialog.openInformation(HandlerUtil.getActiveShell(event),
-					"Information", "Please select a PHP source file");
-			return null;
-		}
-
-		if (null == getExecutable()) {
-			MessageDialog.openInformation(HandlerUtil.getActiveShell(event),
-					"Information",
-					"Please setup phpunit path in plugin preferences");
-			return null;
-		}
-
 		try {
-			Job testJob = new PHPTestJob(
-					view, 
+			assertIsNotAlreadyRunning();
+	
+			IModelElement unit = getModelElement(event);
+
+			new PHPTestJob(
+					getResultView(event), 
 					new PHPUnitCommand(unit, ProjectFinder.getProject(unit), getExecutable()),
 					analyzer
-			);
-			
-			testJob.schedule();
-		} catch (ProjectNotFoundException e) {
+			).schedule();
+
+		} catch (Throwable e) {
 			MessageDialog.openInformation(HandlerUtil.getActiveShell(event),
 					"Information", e.getMessage());
-		}
+		} 
 
 		return null;
 	}
 
-	private ResultView getResultView(ExecutionEvent event) {
+	private ResultView getResultView(ExecutionEvent event) throws UnableToHandleException {
 		try {
 			return (ResultView) HandlerUtil.getActiveWorkbenchWindow(event)
 					.getActivePage().showView(ResultView.ID, null,
 							IWorkbenchPage.VIEW_VISIBLE);
 
 		} catch (PartInitException e) {
-			MessageDialog.openError(HandlerUtil.getActiveShell(event), "Error",
-					"Error opening Result View " + e.getMessage());
-			return null;
+			throw new UnableToHandleException("Error opening Result View " + e.getMessage());
 		}
 	}
 
-	private boolean isAlreadyRunning() {
-		return Job.getJobManager().find(PHPTestJob.FAMILY).length > 0;
+	private void assertIsNotAlreadyRunning() throws UnableToHandleException {
+		if (Job.getJobManager().find(PHPTestJob.FAMILY).length > 0) {
+			throw new UnableToHandleException("PHPUnit tests is already running");
+		}
 	}
 	
-	private String getExecutable() {
+	private String getExecutable() throws UnableToHandleException {
 		String executable = PHPUnitPlugin.getDefault().getPreferenceStore().getString(
 				PHPUnitPlugin.EXECUTABLE);
 		
-		if (executable.equals("")) {
-			return null;
+		if (executable.equals("") || null == executable) {
+			throw new UnableToHandleException("Please setup phpunit path in plugin preferences");
 		}
 		
 		return executable;
 	}
 	
-	private IModelElement getSelection(ExecutionEvent event) {
-		IStructuredSelection selection = (IStructuredSelection) HandlerUtil
-				.getActiveMenuSelection(event);
+	private IModelElement getModelElement(ExecutionEvent event) throws UnableToHandleException {
+		try {
+			return getFromSelection(event);
+		} catch (Throwable e) {
+			try {
+				return getFromActiveEditor(event);
+			} catch (Throwable e2) {
+				throw new UnableToHandleException("Unable to get selection: "  + e.getMessage());
+			}		
+		}
+	}
+	
+	private IModelElement getFromSelection(ExecutionEvent event) throws UnableToHandleException {
+		Object element = getSelection(event).getFirstElement(); 
+		
+		if (null == element) {
+			throw new UnableToHandleException("Not found in selection");
+		}
+		
+		return (IModelElement)element;
+	}
+	
+	private IModelElement getFromActiveEditor(ExecutionEvent event) throws UnableToHandleException {
+		return getActiveEditor(event).getModelElement();
+	}
+	
+	private PHPStructuredEditor getActiveEditor(ExecutionEvent event) throws UnableToHandleException {
+		IEditorPart editor = PHPUnitPlugin.getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
 
 		try {
-			Object elem = null;
-
-			if (null != selection) {
-				elem = selection.getFirstElement();
-			}
-
-			if (null == selection
-					&& event.getApplicationContext() instanceof IEvaluationContext) {
-				IEvaluationContext ctx = (IEvaluationContext) event
-						.getApplicationContext();
-
-				Object defaultVariables = ctx.getDefaultVariable();
-				elem = ((List<?>)defaultVariables).get(0);
-			}
-
-			// dependency on internal PDT class
-			if (elem instanceof IImplForPhp) {
-				return ((IImplForPhp) elem).getModelElement();
-			}
-
-			return (IModelElement) elem;
-		} catch (Exception e) {
-			return null;
+			return (PHPStructuredEditor)editor;
+		} catch (ClassCastException e) {
+			throw new UnableToHandleException("non-php active editor");
 		}
+	}
+	
+	private IStructuredSelection getSelection(ExecutionEvent event) {
+		return (IStructuredSelection) HandlerUtil.getActiveMenuSelection(event);
 	}
 }
---
+++ plugin/src/me/galkin/eclipse/php/handler/UnableToHandleException.java
@@ -0,0 +1,10 @@
+package me.galkin.eclipse.php.handler;
+
+class UnableToHandleException extends Exception {
+
+	private static final long serialVersionUID = 8707545587285251894L;
+	
+	public UnableToHandleException(String string) {
+		super(string);
+	}
+}
